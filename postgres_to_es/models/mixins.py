9b9038f2a0ac4201f8079fa4e7c22dcd516e6576
Tim <tatarovtk@gmail.com>
2026-06-23 17:23:02 +0300

    добавил etl фильмов из postgres в elastic

---
+++ postgres_to_es/models/mixins.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import datetime
+
+from pydantic import BaseModel
+
+
+class IdMixIn(BaseModel):
+    id: str
+
+
+class UpdatedAtMixin(BaseModel):
+    updated_at: str | datetime.datetime
